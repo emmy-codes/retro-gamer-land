3ee5ae0aa80d13e6cd28c960103b35f25c457d52
Emmy_codes <alunia.moon@gmail.com>
2021-08-05 17:42:33 +0200

    first overhaul

---
+++ src/Components/Clippy.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../App.css";
+
+function Clippy() {
+  return (
+    <>
+      <div className="clippycontent">
+        <div className="clippy-balloon icon-text">
+          Hello! It looks like you're visiting Emma's portfolio for the first
+          time. Try clicking on any of the icons and see what happens!
+        </div>
+        <div className="clippy"></div>
+      </div>
+    </>
+  );
+};
+
+export default Clippy;
