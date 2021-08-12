d2699637c0607a681b7e96a860bbe6fef4b6246b
Emmy_codes <alunia.moon@gmail.com>
2021-08-12 14:49:24 +0200

    fiddling with start menu css

--- src/Components/StartMenu.js
+++ src/Components/StartMenu.js
@@ -1,9 +1,47 @@
-import React from "react";
+import React, {useRef} from "react";
 
 function StartMenu() {
+  let startMenu = useRef(null);
+  let openStartMenu = useRef(null);
+
+  function openMenu() {
+    if (startMenu.current.classList.contains("hidden")) {
+      startMenu.current.classList.remove("hidden");
+      openStartMenu.current.classList.add("clicked-start-button");
+    } else {
+      startMenu.current.classList.add("hidden");
+      openStartMenu.current.classList.remove("clicked-start-button");
+    }
+  }
   return (
     <>
-      <div className="start-button win-icon">Start</div>
+      <div
+        className="start-button win-icon"
+        ref={openStartMenu}
+        onClick={openMenu}
+      >
+        Start
+      </div>
+
+      <div className="start-menu hidden" ref={startMenu}>
+        <div className="side-bar">
+          <p className="side-bar-text">
+            Windows <span class="nr95">95</span>
+          </p>
+        </div>
+
+        <div className="menu-items">
+          <div className="menu-item folder-img" id="documents">
+            Documents
+          </div>
+          <div className="menu-item folder-img" id="documents">
+            Fake Folder
+          </div>
+          <div className="menu-item folder-img" id="documents">
+            Fake Folder 2
+          </div>
+          </div>
+        </div>
     </>
   );
 }
--- src/Components/TheInternet.js
+++ src/Components/TheInternet.js
@@ -6,6 +6,7 @@ function TheInternet() {
       <a
         href="https://www.linkedin.com/in/emma-scott-allen-86b7a6140/"
         target="_blank"
+        rel="noreferrer"
         className="link-details all-icons icon-text the-internet-icon"
       >
         The Internet
