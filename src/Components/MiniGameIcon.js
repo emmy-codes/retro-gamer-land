c024d79d3c4da12164108e5254afe57a3b882bc6
Emmy_codes <alunia.moon@gmail.com>
2021-08-29 15:49:34 +0200

    updated link for game

--- src/Components/MiniGameIcon.js
+++ src/Components/MiniGameIcon.js
@@ -4,7 +4,9 @@ import { Link } from "react-router-dom";
 function MiniGameIcon() {
   return (
     <>
-      <div className="mini-game icon-text all-icons">Retro Gamer Land</div>
+      <Link to="/retro-gamer-land" className="mini-game icon-text all-icons">
+        Retro Gamer Land
+      </Link>
     </>
   );
 }
