f1730df7ec2418508a69b354d93fe12f2494010d
Emmy_codes <alunia.moon@gmail.com>
2021-09-13 22:11:53 +0200

    banish clippy to the nether realm

--- src/Components/Clippy.js
+++ src/Components/Clippy.js
@@ -1,17 +1,33 @@
 import React from "react";
 
 function Clippy() {
-  return (
+  const clippyText = (
+    <div className="clippy-balloon icon-text">
+      Hello! <br />
+      It looks like you're visiting Emma's portfolio for the first time. Try
+      clicking on any of the icons and see what happens!
+    </div>
+  );
+
+  const [showOrHide, updateShowOrHide] = React.useState(true);
+
+  function fkOffClippy() {
+    updateShowOrHide(false);
+  };
+
+  return showOrHide ? (
     <>
       <div className="clippycontent">
-        <div className="clippy-balloon icon-text">
-          Hello! It looks like you're visiting Emma's portfolio for the first
-          time. Try clicking on any of the icons and see what happens!
-        </div>
+        {
+          clippyText /* pointless but showing how to store components in variables */
+        }
         <div className="clippy"></div>
+        <button className="close" onClick={fkOffClippy}>
+          x
+        </button>
       </div>
     </>
-  );
-};
+  ) : null;
+}
 
 export default Clippy;
