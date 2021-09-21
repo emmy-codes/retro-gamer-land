40d8a4ede1c1de876575c953b4847b6c14aed2d1
Emmy_codes <alunia.moon@gmail.com>
2021-09-21 20:34:00 +0200

    created collision detection in miniGame /o/

--- src/Components/Character.js
+++ src/Components/Character.js
@@ -1,10 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
 import styled from "styled-components";
 
+let underneathBlock;
+
 // removed empty array from argument {}
-function Character() {
+function Character(props) {
   const [x, setX] = useState(0);
   const [y, setY] = useState(70);
+  const [blocksPosition, setBlocksPosition] = useState([]);
+  const character = useRef();
   const Sprite = styled.div`
     background-color: #5261eb;
     outline: dotted;
@@ -23,32 +27,59 @@ function Character() {
   // eslint-disable-next-line
   function movement(event) {
     const actionX = actionXMap[event.key];
-    
+
     actionX && setX(actionX);
     if (event.key === "ArrowUp") {
-      setY(y - 20)
-      setTimeout(() => {setY(y)},200)
+      setY(y - 20);
+      setTimeout(() => {
+        alert(`Hurrah, you reached ${underneathBlock.classList.value}`)
+        setY(y);
+      }, 200);
     }
-  };
+  }
 
   useEffect(() => {
     document.addEventListener("keydown", movement);
+    const positions = [];
+
+    props.blocks.forEach((currentBlock) => {
+        const currentPosition = currentBlock.current.getBoundingClientRect();
+        positions.push(currentPosition)
+    });
+
+    setBlocksPosition(positions)
+
     // eslint-disable-next-line
-  }, []);
+  }, []); // empty array means useEffect only runs once rather than every time the component updates https://reactjs.org/docs/hooks-effect.html#explanation-why-effects-run-on-each-update
+
+  useLayoutEffect(() => {
+    const currentPosition = character.current.getBoundingClientRect();
+
+    for(let index = 0; index < blocksPosition.length; index++){
+      const currentBlock = blocksPosition[index];
+      if(currentBlock.x === currentPosition.x) {
+        underneathBlock = props.blocks[index].current;
+        break;
+      } else {
+        underneathBlock = null;
+      }
+
+    }
+  })
 
   function increment(x) {
     return x + 5;
-  };
+  }
 
   function decrement(x) {
     return x - 5;
-  };
+  }
 
   return (
     <div onKeyPress={movement}>
-      <Sprite x={x} y={y}></Sprite>
+      <Sprite ref={character} x={x} y={y}></Sprite>
     </div>
   );
-};
+}
 
 export default Character;
--- src/Components/MiniGame.js
+++ src/Components/MiniGame.js
@@ -2,15 +2,21 @@ import React from "react";
 import Character from "./Character";
 
 function MiniGame() {
+  const block1 = React.useRef();
+  const block2 = React.useRef();
+  const block3 = React.useRef();
+
+  const blocks = [block1, block2, block3];
+
   return (
     <>
       <div className="main-stage">
         <div className="ground"></div>
-        <div className="block1"></div>
-        <div className="block2"></div>
-        <div className="block3"></div>
+        <div className="block1" ref={block1}></div>
+        <div className="block2" ref={block2}></div>
+        <div className="block3" ref={block3}></div>
       </div>
-      <Character />
+      <Character blocks={blocks} />
     </>
   );
 };
