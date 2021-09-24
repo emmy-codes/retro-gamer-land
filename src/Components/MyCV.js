e29fd57bb3196d4e70e02be9111112ce9c83b4c7
Emmy_codes <alunia.moon@gmail.com>
2021-09-24 12:42:14 +0200

    cv now visible

--- src/Components/MyCV.js
+++ src/Components/MyCV.js
@@ -1,12 +1,15 @@
 import React from "react";
 import CV from "../Images/CV.jpg";
 
-function MyCV() {
-    return(
-        <>
-            <img src={CV} />
-        </>
-    )
+function MyCV(props) {
+  return (
+    <div className="cv-wrapper">
+      <button className="close" onClick={props.close}>
+        x
+      </button>
+      <img src={CV} />
+    </div>
+  );
 }
 
 export default MyCV;
